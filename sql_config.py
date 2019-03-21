e0df6de0f146a9f829a907bf3f33be264005189b
brentleeper <36904623+brentleeper@users.noreply.github.com>
2019-03-21 10:51:02 -0500

    Create sql_config.py

---
+++ sql_config.py
@@ -0,0 +1,11 @@
+
+# configure sql connections here
+# reference: https://docs.sqlalchemy.org/en/latest/core/engines.html
+
+psql_target_conn_str = None # REQUIRED!
+#psql_target_conn_str = "postgresql+psycopg2://username:password@host:port/database"
+
+psql_source_conn_str = None
+mysql_source_conn_str = None
+
+
